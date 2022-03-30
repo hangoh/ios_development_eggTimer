1cec8f07cbc8f46404ca410952f69c5cb3d75212
hangoh <gohyuhan12345@gmail.com>
2022-03-30 23:58:58 +0900

    Initial Commit

---
+++ egg_timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  egg_timer
+//
+//  Created by Goh Yuhan on 2022/03/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
